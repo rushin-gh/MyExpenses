52348977f75c4e540150c8a462506b37e4fc7523
rushin-gh <rushin.mailme@gmail.com>
2025-07-08 00:27:58 +0530

    Expense table UI part done

---
+++ MyExpenses/MyExpenses/Controllers/ExpenseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyExpenses.Models;
+
+namespace MyExpenses.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var expenses = new List<Expense>()
+            {
+                new Expense("Spectacle", "Bought Spectacle Frame", 1300),
+                new Expense("Bike repair", "Headlamp cover, Garnish, Leg Guard", 1800),
+                new Expense("Raincoat", 149),
+                new Expense("Bike wash", "Diesel wash", 120)
+            };
+            return View(expenses);
+        }
+    }
+}
---
+++ MyExpenses/MyExpenses/Models/Expense.cs
@@ -0,0 +1,20 @@
+﻿namespace MyExpenses.Models
+{
+    public class Expense
+    {
+        public string Title { get; set; }
+
+        public string Desc { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public Expense(string title, string desc, decimal amount)
+        {
+            Title = title;
+            Desc = desc;
+            Amount = amount;
+        }
+
+        public Expense(string title, decimal amount) : this(title, string.Empty, amount) { }
+    }
+}
