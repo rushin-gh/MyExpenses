b8b91c834a618c6f41a51e26667e5886906d864c
rushin-gh <rushin.mailme@gmail.com>
2025-07-10 00:21:54 +0530

    Created SP for getting user expense

--- MyExpenses/MyExpenses/AppQueries.sql
+++ MyExpenses/MyExpenses/AppQueries.sql
@@ -1,3 +1,5 @@
+--Use MyExpenseDB;
+
 --create table Users (
 --	UserId int primary key,
 --	Username varchar(30),
@@ -12,3 +14,28 @@
 --	UserId int,
 --	Constraint FK_Users_Expenses Foreign Key (UserId) References Users(UserId)
 --);
+
+
+--select * from Users;
+
+--insert into Expenses values
+--('Visit to Dmart', 'Shoes, Raincoat, Wheel, Crocks', 1930, 1)
+
+--select * from Expenses
+
+--sp_help Users
+
+--Create or alter procedure GetUserExpenses
+--	@UserId INT
+--AS
+--BEGIN
+--	Select Title, [Desc], Amount FROM Expenses where UserId = @UserId;
+--END
+
+--GetUserExpenses @UserId = 1;
+
+
+--insert into Expenses values
+--('Laundry', 'White shirt dry cleaning', 80, 1),
+--('Iphone 16 Pro', '128 GB variant', 104000, 1),
+--('Shine', 'BSIV', 80000, 1);
